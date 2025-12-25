ca5ba201f4b7a2d8cfb7d229c4c24115e2035ef2
qJasonm <qmjas022@gmail.com>
2025-12-25 13:26:39 -0600

    updated index.js to work with React 19

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './stores/store';
-import App from './App'; 
-import './styles.css'
+import App from './App';
+import './styles.css';
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
--- frontend/src/stores/store.ts
+++ frontend/src/stores/store.ts
@@ -1,15 +1,17 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
+
 import newsReducer from "../reducers/NewsReducer";
 import blogsReducer from "../reducers/BlogsReducer";
 
-const middlewares = [thunk];
-
 const rootReducer = combineReducers({
-    news: newsReducer,
-    blogs: blogsReducer,
-  });
-  
-  const store = createStore(rootReducer, applyMiddleware(thunk));
+  news: newsReducer,
+  blogs: blogsReducer,
+});
 
-  export default store;
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
+
+export default store;
